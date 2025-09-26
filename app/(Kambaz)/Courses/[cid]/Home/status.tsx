9261dbcf9b0f683eb85e6e0ad65c5fee6b1466d7
TheSalmanShariff <99094248+TheSalmanShariff@users.noreply.github.com>
2025-09-26 12:47:40 -0400

    Update status.tsx

--- app/(Kambaz)/Courses/[cid]/Home/status.tsx
+++ app/(Kambaz)/Courses/[cid]/Home/status.tsx
@@ -1,16 +1,21 @@
 export default function CourseStatus() {
   return (
-    <div id="wd-course-status">
-      <h2>Course Status</h2>
-      <button>Unpublish</button> <button>Publish</button>
-      <br /><br />
-      <button>Import Existing Content</button><br />
-      <button>Import from Commons</button><br />
-      <button>Choose Home Page</button><br />
-      <button>View Course Stream</button><br />
-      <button>New Announcement</button><br />
-      <button>New Analytics</button><br />
-      <button>View Course Notifications</button>
+    <div id="wd-course-status" className="card">
+      <div className="card-body">
+        <h5 className="card-title">Course Status</h5>
+        <div className="d-grid gap-2">
+          <button className="btn btn-outline-secondary">Unpublish</button>
+          <button className="btn btn-success">Publish</button>
+          <hr />
+          <button className="btn btn-outline-primary">Import Existing Content</button>
+          <button className="btn btn-outline-primary">Import from Commons</button>
+          <button className="btn btn-outline-primary">Choose Home Page</button>
+          <button className="btn btn-outline-primary">View Course Stream</button>
+          <button className="btn btn-outline-primary">New Announcement</button>
+          <button className="btn btn-outline-primary">New Analytics</button>
+          <button className="btn btn-outline-primary">View Course Notifications</button>
+        </div>
+      </div>
     </div>
   );
 }
