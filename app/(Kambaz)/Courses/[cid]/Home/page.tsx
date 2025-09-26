bf8d5d07006668af24740dbe053cef3729346b52
TheSalmanShariff <99094248+TheSalmanShariff@users.noreply.github.com>
2025-09-26 12:58:20 -0400

    page.tsx

--- app/(Kambaz)/Courses/[cid]/Home/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Home/page.tsx
@@ -1,29 +1,35 @@
 import Modules from "../Modules/page";
-import CourseStatus from "./Status";
 
 export default function Home() {
   return (
     <div id="wd-home">
-      {/* Buttons at the top */}
-      <div style={{ marginBottom: '20px' }}>
-        <button>Collapse All</button>
-        <button>View Progress</button>
-        <button>Publish All</button>
-        <button>+ Module</button>
+      <div className="d-flex justify-content-end mb-3">
+        <button className="btn btn-secondary me-2">Collapse All</button>
+        <button className="btn btn-secondary me-2">View Progress</button>
+        <button className="btn btn-danger">+ Module</button>
       </div>
       
-      <table>
-        <tbody>
-          <tr>
-            <td valign="top" width="70%"> 
-              <Modules /> 
-            </td>
-            <td valign="top"> 
-              <CourseStatus /> 
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div className="row">
+        <div className="col-8">
+          <Modules />
+        </div>
+        <div className="col-4">
+          <div id="wd-course-status" className="card">
+            <div className="card-body">
+              <h5 className="card-title">Course Status</h5>
+              <button className="btn btn-outline-secondary me-2">Unpublish</button>
+              <button className="btn btn-success">Publish</button>
+              <hr />
+              <button className="btn btn-outline-primary d-block mb-2">Import Existing Content</button>
+              <button className="btn btn-outline-primary d-block mb-2">Import from Commons</button>
+              <button className="btn btn-outline-primary d-block mb-2">Choose Home Page</button>
+              <button className="btn btn-outline-primary d-block mb-2">View Course Stream</button>
+              <button className="btn btn-outline-primary d-block mb-2">New Announcement</button>
+              <button className="btn btn-outline-primary d-block">View Course Notifications</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
